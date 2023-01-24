c198b5a96854ff1762c1d391cc37d3b18f038127
franciscobarriola1 <franciscobarriola1@gmail.com>
2023-01-24 16:44:58 -0300

    Segunda Entra 1.1

--- main.js
+++ main.js
@@ -49,10 +49,15 @@ function nuevaVestimenta(){
 
 function buscarTalle(){
     alert('Talles son : small, medium, large, XL , XXL')
+    let talleSeleccionado = prompt("Elija su talle:")
     let resultado = camisetas.find(obj => {
-        return obj.talle === prompt("Elija su talle:")
+        return obj.talle === talleSeleccionado;
     })
-    console.log(resultado)
+    if(resultado){
+        console.log(resultado)
+    }else{
+        alert('El talle no existe')
+    }
 }
 
 // Uncaught ReferenceError: Cannot access 'ropa' before initializationat precioRopa (main.js:96:21) at main.js:128:1 (resuelto)
@@ -109,45 +114,45 @@ function preguntarOpcion(exit){
         }
 
 
-    function realizarCompra(){
-        let nombre = prompt("Ingrese su nombre:")
-        let apellido = prompt ("Ingrese su apellidio")
-        console.log("Bienvenido " + nombre + " " + apellido)
-        let cantidadRopa = parseInt(prompt("Actualmente se encuentra a la venta remeras, pantalones, zapatillas, gorras y medias. ¿Cuantos Articulos desea llevar?"))
-   
-        let total = 0;
-        for (let i=1;  i <= cantidadRopa; i++){
-            let ropa = parseInt(prompt("Selecionar:\n Remera=1\n Pantalones=2\n Zapatillas=3\n Gorra=4\n Medias=5"))
-            
-            switch(ropa){
-                case 1:
-                    console.log("Has agregado 1 remera");
-                    total = total + precioRemera;
-                break;
-                case 2:
-                    console.log("Has agregado 1 pantalón");
-                    total = total + precioPantalones;
-                break;
-                case 3:
-                    console.log("Has agregado 1 par de Zapatillas");
-                    total = total + precioZapatillas;
-                break;
-                case 4:
-                    console.log("Has agregado 1 gorra");
-                    total = total + precioGorra;
-                break;
-                case 5:
-                    console.log("Has agregado 1 par de medias");
-                    total = total + precioMedias;
-                break;
-                default:
-                    console.log("Ingresar una opción definida")
+        function realizarCompra(){
+            let nombre = prompt("Ingrese su nombre:")
+            let apellido = prompt ("Ingrese su apellidio")
+            console.log("Bienvenido " + nombre + " " + apellido)
+            let cantidadRopa = parseInt(prompt("Actualmente se encuentra a la venta remeras, pantalones, zapatillas, gorras y medias. ¿Cuantos Articulos desea llevar?"))
+       
+            let total = 0;
+            for (let i=1;  i <= cantidadRopa; i++){
+                let ropa = parseInt(prompt("Selecionar:\n Remera=1\n Pantalones=2\n Zapatillas=3\n Gorra=4\n Medias=5"))
+                
+                switch(ropa){
+                    case 1:
+                        console.log("Has agregado 1 remera");
+                        total = total + precioRemera;
+                    break;
+                    case 2:
+                        console.log("Has agregado 1 pantalón");
+                        total = total + precioPantalones;
+                    break;
+                    case 3:
+                        console.log("Has agregado 1 par de Zapatillas");
+                        total = total + precioZapatillas;
+                    break;
+                    case 4:
+                        console.log("Has agregado 1 gorra");
+                        total = total + precioGorra;
+                    break;
+                    case 5:
+                        console.log("Has agregado 1 par de medias");
+                        total = total + precioMedias;
+                    break;
+                    default:
+                        console.log("Ingresar una opción definida")
+                }
             }
+            
+            console.log(nombre + " " + apellido + " " + "su total igual a: " + total + "$")
+            
         }
-        
-        console.log(nombre + " " + apellido + " " + "su total igual a: " + total + "$")
-        
-    }
 
 
  preguntarOpcion();
